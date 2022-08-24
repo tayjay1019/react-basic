827e74539af3cce2c38296618ebd03d87e27940d
tayjay1019 <57647217+tayjay1019@users.noreply.github.com>
2022-08-24 01:39:55 -0500

    Added Medal component and other countries

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
-import { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react';
 import Country from './components/Country';
+import './App.css';
 
 class App extends Component {
   state = {
     countries: [
-      {id: 1, name: 'United States', count: 0},
-      {id: 2, name: 'China', count: 0},
-      {id: 3, name: 'Germany', count: 0},
+      { id: 1, name: 'United States', gold: 0, silver: 0, bronze: 0 },
+      { id: 2, name: 'China', gold: 0, silver: 0, bronze: 0 },
+      { id: 3, name: 'Germany', gold: 0, silver: 0, bronze: 0 },
+    ],
+    medals: [
+      { id: 1, name: 'gold' },
+      { id: 2, name: 'silver' },
+      { id: 3, name: 'bronze' },
     ]
   }
-  handleClick = (countryId) => {
-    this.setState( prevState => ({
-      countries: prevState.countries.map(
-        el => el.id === countryId? { ...el, count: (el.count +1)}: el
-      )
-    }))
-}
- render() {
-  return (
-    <div className="App"> 
-        <header>
-          Medal Count
-        </header>
-        { this.state.countries.map(country =>
-          <Country
-            key={ country.id }
-            country={ country } 
-            onClick={ this.handleClick }/>
+  handleIncrement = (countryId, medalName) => {
+    const countries = [ ...this.state.countries ];
+    const idx = countries.findIndex(c => c.id === countryId);
+    countries[idx][medalName] += 1;
+    this.setState({ countries: countries });
+  }
+  handleDecrement = (countryId, medalName) => {
+    const countries = [ ...this.state.countries ];
+    const idx = countries.findIndex(c => c.id === countryId);
+    countries[idx][medalName] -= 1;
+    this.setState({ countries: countries });
+  }
+  getAllMedalsTotal() {
+    let sum = 0;
+    this.state.medals.forEach(medal => { sum += this.state.countries.reduce((a, b) => a + b[medal.name], 0); });
+    return sum;
+  }
+  render() { 
+    return (
+      <div className="App">
+        <div className='appHeading'>
+          Olympic Medals
+          <span className='badge'>
+            { this.getAllMedalsTotal() }
+          </span>
+        </div>
+        <div className='countries'>
+            { this.state.countries.map(country => 
+              <Country 
+                key={ country.id } 
+                country={ country } 
+                medals={ this.state.medals }
+                onIncrement={ this.handleIncrement } 
+                onDecrement={ this.handleDecrement } />
             )}
+        </div>
       </div>
-    );  
+    );
   }
 }
-
+ 
 export default App;
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import Medal from './Medal';
 
 class Country extends Component {
-    render() {
-        const { onClick, country } = this.props;
-        return (
-            <div className='Country'>
-                 <div>   {country.name}  </div>
-                 <div>Gold medals: {country.count} <button onClick={ () => onClick(country.id) }> + </button></div> 
-            </div>
-        )
-    }
+  getMedalsTotal(country, medals) {
+    let sum = 0;
+    medals.forEach(medal => { sum += country[medal.name]; });
+    return sum;
+  }
+  render() { 
+    const { country, medals, onIncrement, onDecrement } = this.props;
+    return (
+      <div className="country">
+        <div className="name">
+          { country.name }
+          <span className="badge">
+            { this.getMedalsTotal(country, medals) }
+          </span>
+        </div>
+        { medals.map(medal =>
+          <Medal 
+            key={ medal.id } 
+            country={ country } 
+            medal={ medal } 
+            onIncrement={ onIncrement } 
+            onDecrement={ onDecrement } />
+        ) }
+        <hr />
+      </div>
+    );
+  }
 }
 
 export default Country;
